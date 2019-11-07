6594d74a1882d5448e90b0cba0fd43398cb298cf
lakehanne <patlekano@gmail.com>
2019-11-07 10:01:43 -0500

    maybe

--- papermit.sh
+++ papermit.sh
@@ -2,8 +2,11 @@
 
 # commits kinecontrol and model and other working papers
 
-pdftk ../Papers/SRS/Continuum/kinecontrol/kinecontrol.pdf output  downloads/Papers/kinecontrol.pdf  user_pw semaphore
+cd downloads/Papers
+pdftk ~/Documents/Papers/SRS/Continuum/kinecontrol/kinecontrol.pdf output kinecontrol.pdf  user_pw semaphore
 
-pdftk ../Papers/SRS/Continuum/model/model.pdf output downloads/Papers/ContinuumI.pdf
+cp ~/Documents/Papers/SRS/Continuum/model/model.pdf ContinuumI.pdf
+
+cd ../../
 
 commit
