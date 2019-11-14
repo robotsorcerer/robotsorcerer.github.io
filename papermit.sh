df73994e49b2a84c746edc27ce40e12330beb5e2
lakehanne <patlekano@gmail.com>
2019-11-14 09:28:13 -0500

    new continuumii

--- papermit.sh
+++ papermit.sh
@@ -3,9 +3,8 @@
 # commits kinecontrol and model and other working papers
 
 cd downloads/Papers
-pdftk ~/Documents/Papers/SRS/Continuum/kinecontrol/kinecontrol.pdf output kinecontrol.pdf  user_pw semaphore
-
-
+#pdftk ~/Documents/Papers/SRS/Continuum/kinecontrol/kinecontrol.pdf output kinecontrol.pdf  user_pw semaphore
+cp ~/Documents/Papers/SRS/Continuum/kinecontrol/kinecontrol.pdf kinecontrol.pdf  
 cp ~/Documents/Papers/SRS/Springer2019/springer19.pdf Springer19.pdf
 
 cp ~/Documents/Papers/SRS/Continuum/model/model.pdf ContinuumI.pdf
