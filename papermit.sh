115116dced15435e262d73e860611ec0b3c40fff
lakehanne <patlekano@gmail.com>
2019-11-26 14:47:39 -0500

    all locked

--- papermit.sh
+++ papermit.sh
@@ -3,11 +3,11 @@
 # commits kinecontrol and model and other working papers
 
 cd downloads/Papers
-#pdftk ~/Documents/Papers/SRS/Continuum/kinecontrol/kinecontrol.pdf output kinecontrol.pdf  user_pw semaphore
-cp ~/Documents/Papers/SRS/Continuum/kinecontrol/kinecontrol.pdf kinecontrol.pdf  
-cp ~/Documents/Papers/SRS/Springer2019/springer19.pdf Springer19.pdf
+pdftk ~/Documents/Papers/SRS/Continuum/kinecontrol/kinecontrol.pdf output kinecontrol.pdf  user_pw semaphore
 
-cp ~/Documents/Papers/SRS/Continuum/model/model.pdf ContinuumI.pdf
+pdftk ~/Documents/Papers/SRS/Springer2019/springer19.pdf output Springer19.pdf user_pw spring_it
+
+pdftk ~/Documents/Papers/SRS/Continuum/model/model.pdf output ContinuumI.pdf user_pw continua
 
 cd ../../
 
