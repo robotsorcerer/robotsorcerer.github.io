ba4c623f84c678bf3bf01b5f00e620fd46ab0ad8
lakehanne <patlekano@gmail.com>
2019-12-09 00:36:31 -0500

    local commits

--- papermit.sh
+++ papermit.sh
@@ -4,13 +4,13 @@
 
 cd downloads/Papers
 
-#pdftk ~/Documents/Papers/SRS/Continuum/kinecontrol/kinecontrol.pdf output kinecontrol.pdf  user_pw semaphore
+pdftk ~/Documents/Papers/SRS/Continuum/kinecontrol/kinecontrol.pdf output kinecontrol.pdf  user_pw semaphore
 #
 pdftk ~/Documents/Papers/SRS/Springer2019/springer19.pdf output Springer19.pdf user_pw spring_it
 #
-#pdftk ~/Documents/Papers/SRS/Continuum/model/model.pdf output ContinuumI.pdf user_pw continua
+pdftk ~/Documents/Papers/SRS/Continuum/model/model.pdf output ContinuumI.pdf user_pw continua
 #
-cp ~/Documents/Papers/SRS/Continuum/kinecontrol/kinecontrol.pdf kinecontrol.pdf  
+#cp ~/Documents/Papers/SRS/Continuum/kinecontrol/kinecontrol.pdf kinecontrol.pdf  
 #cp ~/Documents/Papers/SRS/Springer2019/springer19.pdf Springer19.pdf 
 #
 #cp ~/Documents/Papers/SRS/Continuum/model/model.pdf ContinuumI.pdf
